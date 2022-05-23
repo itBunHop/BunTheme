c7e6aebbcc48b818b9d7988be30539cc5ddc71b1
Bun <53990462+itBunHop@users.noreply.github.com>
2022-05-23 20:23:51 +1000

    Update Colors.lua
    apparently i forgot

--- Scripts/Colors.lua
+++ Scripts/Colors.lua
@@ -20,13 +20,26 @@ function ContrastingDifficultyColor( dc )
 	return "0,0,0,1"
 end
 
-function JudgmentColor(jud)
+function JudgmentColorHarsh(jud)
 	-- order is fantastic, excellent, great, decent, way off, miss
+	-- harsh variant (more saturated)
 	if jud == 1 then return "#32C8FA" end
 	if jud == 2 then return "#FAC832" end
 	if jud == 3 then return "#32FA64" end
 	if jud == 4 then return "#DC78FF" end
 	if jud == 5 then return "#FA8C64" end
 	if jud == 6 then return "#FA4040" end
+	return "#FFFFFF"
+end
+
+function JudgmentColorLight(jud)
+	-- order is fantastic, excellent, great, decent, way off, miss
+	-- light variant (less saturated)
+	if jud == 1 then return "#96DCFA" end
+	if jud == 2 then return "#FADC64" end
+	if jud == 3 then return "#64FA96" end
+	if jud == 4 then return "#E6A0FF" end
+	if jud == 5 then return "#FAB496" end
+	if jud == 6 then return "#FA7878" end
 	return "#FFFFFF"
 end
